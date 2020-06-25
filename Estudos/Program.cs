6563ff748d43adda79ae98ae589a743e8c5cd00a
Luiz Fernando <luiz.fernando@engegraph.com.br>
2020-06-25 09:47:38 -0300

    Commit - Estudo de Métodos - 24/06/2020

---
+++ Estudos/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tipo  variavel    operador de instanciacao/ associacao
+            Metodos instancia = new Metodos();
+            instancia.Executar();
+            instancia.ExecutarComParametro("Olá mundo");
+
+            int numero1 = 10;
+            int numero2 = 10;
+
+            instancia.Somar(numero1, numero2);
+
+            int numero3 = 10;
+
+            instancia.SomarComDefault(numero1, numero2);
+
+            var numero_E_Par = instancia.NumeroPar(numero1);
+
+            Console.WriteLine(string.Format("O número {0} é {1}", numero1, (numero_E_Par ? "Par" : "Ímpar"))); //usando format
+
+            //Console.WriteLine($"O número {numero1} é {(numero_E_Par ? "Par" : "Ímpar")}"); //usando interpolação
+
+            //Console.WriteLine("O número " + numero1 + " é " + (numero_E_Par ? "Par" : "Ímpar")); //conactenação simples
+
+            Console.WriteLine(string.Concat("Método ObterSoma = ", instancia.ObterSoma(numero1, numero2)));
+
+            int valor = 10;
+
+            instancia.SomarValorPorReferencia(numero1, numero2, ref valor);
+
+            Console.WriteLine($"O Valor = {valor}");
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
